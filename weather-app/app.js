9633491a81c034a9d69ea0e934892c13beae21cc
aslaha@microsoft.com <ashislaha@ASHISs-MacBook-Pro.local>
2024-04-29 23:17:09 -0700

    working on weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,47 @@
 
 const request = require('request');
 
+// https://weatherstack.com/documentation
 const weatherApiKey = "3f2b4de4adf7e149d5839bfd4ef7eccc";
-const requestURL = "http://api.weatherstack.com/current?access_key=3f2b4de4adf7e149d5839bfd4ef7eccc&query=Seattle"
+const weatherRequestURL = "http://api.weatherstack.com/current?access_key=3f2b4de4adf7e149d5839bfd4ef7eccc&query=Seattle"
 
-request.get({url: requestURL, json: true}, (error, response, body) => {
-    if (error) {
-        console.log('error', error);
-    } else {
-        console.log('status code', response.statusCode);
-        const currentTemperature = body.current.temperature;
-        const precip = body.current.precip;
-        const formattedStr = "It's currently " + `${currentTemperature}` + " degree C outside. There is " + `${precip}` + "% chance of rain"
-        console.log(formattedStr);
-    }
-});
+// https://account.mapbox.com/
+const mapBoxEndPoint = "https://api.mapbox.com/search/geocode/v6/forward"
+const mapBoxAccessToken = "pk.eyJ1IjoiYXNoaXNsYWhhIiwiYSI6ImNsdmx5azdpdzJsZzYydm4xd25ieWJ0ZzYifQ.G4WPdCr07mRucWgNXewhLg"
+const language = "en"
+const limit = 1
+
+const getWeather = function() {
+    request.get({url: weatherRequestURL, json: true}, (error, response, body) => {
+        if (error) {
+            console.log('error', error);
+        } else {
+            console.log('status code', response.statusCode);
+            const currentTemperature = body.current.temperature;
+            const precip = body.current.precip;
+            const formattedStr = "It's currently " + `${currentTemperature}` + " degree C outside. There is " + `${precip}` + "% chance of rain"
+            console.log(formattedStr);
+        }
+    });
+}
+
+// geo-coding
+// address (mapbox.com) --> lat, lng --> use api to get weather info
+
+const getForwardGeocoding = (place) => {
+    const url = mapBoxEndPoint + "?q=" + encodeURIComponent(place) + "&access_token=" + mapBoxAccessToken + "&limit=" + `${limit}`
+    console.log(url);
+
+    request.get({url: url, json: true}, (error, response, body) => {
+        if (error) {
+            console.log('error', error);
+        } else {
+            console.log('status code', response.statusCode);
+            const feature = body.features[0];
+            const coordinates = feature.properties.coordinates;
+            console.log(coordinates);
+        }
+    });
+}
+
+getForwardGeocoding("Los angeles")
